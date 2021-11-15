d294302caddd781125a6a72ca805b042f43d2f67
zhangzhenfei <fei@sadais.com>
2021-11-15 10:11:30 +0800

    :art: chore: 初始化项目

--- generator.js
+++ generator.js
@@ -56,6 +56,7 @@ module.exports = (api, options, rootOptions) => {
         // ------------------------- 扩展依赖 End ---------------------------
       },
       devDependencies: {
+        "@babel/runtime": "~7.12.0",// 临时指定版本，7.13.x 会报错
         'postcss-comment': '^2.0.0',
         '@dcloudio/types': '*',
         'miniprogram-api-typings': '*',
@@ -95,30 +96,6 @@ module.exports = (api, options, rootOptions) => {
       // ------------------------- 扩展commit lint End ---------------------------
     }
   })
-  if (options.template === 'default-ts') { // 启用 typescript
-    api.extendPackage(pkg => {
-      return {
-        dependencies: {
-          'vue-class-component': '^6.3.2',
-          'vue-property-decorator': '^8.0.0'
-        },
-        devDependencies: {
-          '@babel/plugin-syntax-typescript': '^7.2.0',
-          '@vue/cli-plugin-typescript': '*',
-          'typescript': api.hasPlugin('eslint') ? '~3.1.1' : '^3.0.0'
-        }
-      }
-    })
-  } else if (options.template === 'dcloudio/uni-template-news') {
-    api.extendPackage(pkg => {
-      return {
-        devDependencies: {
-          'node-sass': '^4.11.0',
-          'sass-loader': '^7.1.0'
-        }
-      }
-    })
-  }
 
   api.render(async function (files) {
     Object.keys(files).forEach(name => {
@@ -131,38 +108,6 @@ module.exports = (api, options, rootOptions) => {
     await generate(path.resolve(__dirname, './template/common'), files)
     if (template === 'default') {
       await generate(path.resolve(__dirname, './template/default'), files, base, rootOptions)
-    } else if (template === 'default-ts') {
-      await generate(path.resolve(__dirname, './template/common-ts'), files)
-      await generate(path.resolve(__dirname, './template/default-ts'), files, base, rootOptions)
-    } else {
-      const ora = require('ora')
-      const home = require('user-home')
-      const download = require('download-git-repo')
-
-      const spinner = ora('模板下载中...')
-      spinner.start()
-
-      const tmp = path.join(home, '.uni-app/templates', template.replace(/[/:]/g, '-'), 'src')
-
-      if (fs.existsSync(tmp)) {
-        try {
-          require('rimraf').sync(tmp)
-        } catch (e) {
-          console.error(e)
-        }
-      }
-
-      await new Promise((resolve, reject) => {
-        download(template, tmp, err => {
-          spinner.stop()
-          if (err) {
-            return reject(err)
-          }
-          resolve()
-        })
-      })
-
-      await generate(tmp, files, base)
     }
   })
 }
